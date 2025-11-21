68a0b34602d510b150ac10fdee150f9cd6ed5f59
hyukjin0419 <21900771@handong.ac.kr>
2025-11-22 04:12:04 +0900

    init

---
+++ src/main/java/com/pard/server/domdry/DomdryApplication.java
@@ -0,0 +1,13 @@
+package com.pard.server.domdry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomdryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomdryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pard/server/domdry/DomdryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pard.server.domdry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomdryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
